ecd178520e8f236313ab47eea1abdabf4597a050
Ye-Yu-Chuan <61823596+Ye-Yu-Chuan@users.noreply.github.com>
2024-06-25 00:13:29 +0800

    Update main.js

--- programming/main.js
+++ programming/main.js
@@ -7,15 +7,15 @@ function selected(){
     const bmr = document.getElementById("bmr").value;
     const floatingSelect = document.getElementById("floatingSelect").value;
 
-    let daily_Total_Cal = 0;
     let daily_TDEE = bmr *1.2;
     let daily_Protein = weight *1.2;
     let daily_Protein_unit = Math.ceil( (daily_Protein -17*2) /8 );
 
+    let daily_Total_Cal = 0;
+    let daily_lunch_Cal = 0;
     if (floatingSelect == "增肌") { daily_Total_Cal = daily_TDEE +500; }
     else if (floatingSelect == "減脂") { daily_Total_Cal = daily_TDEE -500; }
+    daily_lunch_Cal = Math.trunc( daily_Total_Cal -200*2 -daily_Protein_unit*40 );
 
-    daily_Total_Cal = Math.trunc( daily_Total_Cal -200*2 -daily_Protein_unit*40 );
-
-    alert(daily_Total_Cal);
+    alert("中餐攝取熱量上限" + daily_lunch_Cal + "卡\n每日蛋白素至少" + daily_Protein_unit + "匙");
 }
